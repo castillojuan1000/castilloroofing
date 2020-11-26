14693e7fdc38b1715491b377258fc2008f8667af
Juan Castillo <castillojuan1000@gmail.com>
2020-11-26 15:32:25 -0500

    initial commit/setting up the layout

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Header from './components/Header'
+import AboutUs from './components/AboutUs'
+import ChooseUs from './components/ChooseUs'
+import OurWork from './components/OurWork'
+import RequestInspection from './components/RequestInspection'
+
+function App() {
+  return (
+    <>
+    <Header/>
+    <AboutUs/>
+    <ChooseUs/>
+    <OurWork/>
+    <RequestInspection/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/RequestInspection.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function RequestInspection() {
+  return (
+    <>
+      <h1>This is the Request Inspection</h1>
+    </>
+  )
+}
+
+
